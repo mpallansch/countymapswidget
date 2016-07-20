065e55fd5ee4019b190e2835f5bac09118a46b0c
mpallansch <mkp95@uga.edu>
2016-07-20 14:50:22 -0400

    First Commit

---
+++ js/statemapping.js
@@ -0,0 +1,352 @@
+var states = {
+    "01": {
+        name: "Alabama",
+        code: "01",
+        lat: 32.7990,
+        long:-86.8073,
+        scale: 2700
+    },
+    "02": {
+        name: "Alaska",
+        code: "02",
+        lat: 61.3850,
+        long:-152.2683,
+        scale: 300
+    },
+    "04": {
+        name: "Arizona",
+        code: "04",
+        lat: 33.7712,
+        long: -111.3877,
+        scale: 1800
+    },
+    "05": {
+        name: "Arkansas",
+        code: "05",
+        lat: 34.9513,
+        long: -92.3809,
+        scale: 2700
+    },
+    "06": {
+        name: "California",
+        code: "06",
+        lat: 36.1700,
+        long: -119.7462,
+        scale: 1000
+    },
+    "08": {
+        name: "Colorado",
+        code: "08",
+        lat: 39.0646,
+        long: -105.3272,
+        scale: 1800
+    },
+    "09": {
+        name: "Connecticut",
+        code: "09",
+        lat: 41.5834,
+        long: -72.7622,
+        scale: 7000
+    },
+    "10": {
+        name: "Delaware",
+        code: "10",
+        lat: 39.1498,
+        long: -75.5148,
+        scale: 7000
+    },
+    "12": {
+        name: "Florida",
+        code: "12",
+        lat: 27.8333,
+        long: -83.9170,
+        scale: 1700
+    },
+    "13": {
+        name: "Georgia",
+        code: "13",
+        lat: 32.9866,
+        long: -83.6487,
+        scale: 2700
+    },
+    "15": {
+        name: "Hawaii",
+        code: "15",
+        lat: 21.1098,
+        long: -157.5311,
+        scale: 2000
+    },
+    "16": {
+        name: "Idaho",
+        code: "16",
+        lat: 45.7394,
+        long: -114.5103,
+        scale: 1300
+    },
+    "17": {
+        name: "Illinois",
+        code: "17",
+        lat: 39.8363,
+        long: -89.0022,
+        scale: 2000
+    },
+    "18": {
+        name: "Indiana",
+        code: "18",
+        lat: 39.8647,
+        long: -86.2604,
+        scale: 2700
+    },
+    "19": {
+        name: "Iowa",
+        code: "19",
+        lat: 42.0046,
+        long: -93.2140,
+        scale: 2100
+    },
+    "20": {
+        name: "Kansas",
+        code: "20",
+        lat: 38.5111,
+        long: -98.3005,
+        scale: 1800
+    },
+    "21": {
+        name: "Kentucky",
+        code: "21",
+        lat: 37.6690,
+        long: -85.6514,
+        scale: 2000
+    },
+    "22": {
+        name: "Louisiana",
+        code: "22",
+        lat: 31.1801,
+        long: -91.8749,
+        scale: 2300
+    },
+    "23": {
+        name: "Maine",
+        code: "23",
+        lat: 45.3074,
+        long: -69.3977,
+        scale: 2500
+    },
+    "24": {
+        name: "Maryland",
+        code: "24",
+        lat: 39.0724,
+        long: -77.4902,
+        scale: 2700
+    },
+    "25": {
+        name: "Massachusetts",
+        code: "25",
+        lat: 42.2373,
+        long: -71.5314,
+        scale: 2700
+    },
+    "26": {
+        name: "Michigan",
+        code: "26",
+        lat: 43.3504,
+        long: -84.5603,
+        scale: 1300
+    },
+    "27": {
+        name: "Minnesota",
+        code: "27",
+        lat: 46.7326,
+        long: -93.9196,
+        scale: 1600
+    },
+    "28": {
+        name: "Mississippi",
+        code: "28",
+        lat: 32.7673,
+        long: -89.6812,
+        scale: 2700
+    },
+    "29": {
+        name: "Missouri",
+        code: "29",
+        lat: 38.4623,
+        long: -92.3020,
+        scale: 2000
+    },
+    "30": {
+        name: "Montana",
+        code: "30",
+        lat: 46.9048,
+        long: -110.3261,
+        scale: 1100
+    },
+    "31": {
+        name: "Nebraska",
+        code: "31",
+        lat: 41.1289,
+        long: -99.7883,
+        scale: 1600
+    },
+    "32": {
+        name: "Nevada",
+        code: "32",
+        lat: 38.4199,
+        long: -117.1219,
+        scale: 1300
+    },
+    "33": {
+        name: "New Hampshire",
+        code: "33",
+        lat: 43.9108,
+        long: -71.5653,
+        scale: 3700
+    },
+    "34": {
+        name: "New Jersey",
+        code: "34",
+        lat: 40.3140,
+        long: -74.5089,
+        scale: 4000
+    },
+    "35": {
+        name: "New Mexico",
+        code: "35",
+        lat: 34.8375,
+        long: -106.2371,
+        scale: 2000
+    },
+    "36": {
+        name: "New York",
+        code: "36",
+        lat: 43.1497,
+        long: -75.9384,
+        scale: 2000
+    },
+    "37": {
+        name: "North Carolina",
+        code: "37",
+        lat: 35.6411,
+        long: -79.8431,
+        scale: 1600
+    },
+    "38": {
+        name: "North Dakota",
+        code: "38",
+        lat: 47.5362,
+        long: -100.3930,
+        scale: 2000
+    },
+    "39": {
+        name: "Ohio",
+        code: "39",
+        lat: 40.3736,
+        long: -82.7755,
+        scale: 2700
+    },
+    "40": {
+        name: "Oklahoma",
+        code: "40",
+        lat: 35.5376,
+        long: -98.9247,
+        scale: 1600
+    },
+    "41": {
+        name: "Oregon",
+        code: "41",
+        lat: 43.5672,
+        long: -121.1269,
+        scale: 1600
+    },
+    "42": {
+        name: "Pennsylvania",
+        code: "42",
+        lat: 40.5773,
+        long: -77.2640,
+        scale: 2000
+    },
+    "44": {
+        name: "Rhode Island",
+        code: "44",
+        lat: 41.6772,
+        long: -71.5101,
+        scale: 7000
+    },
+    "45": {
+        name: "South Carolina",
+        code: "45",
+        lat: 33.8191,
+        long: -80.9066,
+        scale: 2000
+    },
+    "46": {
+        name: "South Dakota",
+        code: "46",
+        lat: 44.2853,
+        long: -100.4632,
+        scale: 2000
+    },
+    "47": {
+        name: "Tennessee",
+        code: "47",
+        lat: 35.7449,
+        long: -85.7489,
+        scale: 1700
+    },
+    "48": {
+        name: "Texas",
+        code: "48",
+        lat: 31.1060,
+        long: -99.9475,
+        scale: 1000
+    },
+    "49": {
+        name: "Utah",
+        code: "49",
+        lat: 39.1135,
+        long: -111.8535,
+        scale: 2000
+    },
+    "50": {
+        name: "Vermont",
+        code: "50",
+        lat: 44.0407,
+        long: -72.7093,
+        scale: 3700
+    },
+    "51": {
+        name: "Virginia",
+        code: "51",
+        lat: 37.7680,
+        long: -79.2057,
+        scale: 1800
+    },
+    "53": {
+        name: "Washington",
+        code: "53",
+        lat: 47.3917,
+        long: -120.5708,
+        scale: 1900
+    },
+    "54": {
+        name: "West Virginia",
+        code: "54",
+        lat: 38.9680,
+        long: -80.4696,
+        scale: 2700
+    },
+    "55": {
+        name: "Wisconsin",
+        code: "55",
+        lat: 44.7563,
+        long: -89.9385,
+        scale: 2400
+    },
+    "56": {
+        name: "Wyoming",
+        code: "56",
+        lat: 42.7475,
+        long: -107.2085,
+        scale: 1800
+    }
+};
