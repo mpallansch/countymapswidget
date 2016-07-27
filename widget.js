2bca50b5677cebe7d2ac350820dbb8f8a4454b9d
mpallansch <mkp95@uga.edu>
2016-07-27 11:04:18 -0400

    Adds Widget Header Bar

--- widget.js
+++ widget.js
@@ -10,25 +10,12 @@
 		// ADD POINTER/SHORTCUT FOR METRICS
 		window.cdcMetrics = window.cdcCommon.metrics;
 
-		var callParams = window.cdcCommon.runtime.callParams;
-
-		// New Addtions
-		var strCallingPageUrl = window.cdcCommon.getCallParam('chost') + window.cdcCommon.getCallParam('cpath') + window.cdcCommon.getCallParam('csearch');
-		// YOUR VAR NAME = CALLING PAGE HOST + CALLING PAGE PATH
-
-		var strCallingHost = window.cdcCommon.getCallParam('chost');
-
-		var strCallingPageTitle = window.cdcCommon.getCallParam('ctitle');
-		// YOUR VAR NAME = CALLING PAGE TITLE
-
-
-		// SET YOUR EMBED CODE HERE (INT PRE FORMAT [&gt;] instead of [>] etc.)
-		cdcCommon.events.setEmbedCode('&lt;div data-cdc-widget="sampleIframe"&gt;&lt;/div&gt;\n&lt;script src="https://tools.cdc.gov/1M1B"&gt;&lt;/script&gt;');
-
 		// INIT METRICS
 		cdcMetrics.init({
 			useMetrics : 'false'
 		});
+
+		cdcCommon.createEmbedCode(cdcCommon.getCallParam('wn'),['data-instance-name']);
                 
                 // LOAD CONTROLLERS
 		cdcCommon.loadScript('./js/controllers.js', function(){
